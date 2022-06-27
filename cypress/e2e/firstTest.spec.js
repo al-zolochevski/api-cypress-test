c2f1860c0838ed04f83e29d1f466011a97f34e94
al-zolochevski <7061066Alex>
2022-06-27 20:45:52 +0300

    rewrite cy.server() and cy.root() to intercept()

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -3,15 +3,13 @@
 describe('Test with backend', () => {
 
   beforeEach('login to the app', () => {
-    cy.server()
-    cy.route('GET', '**/tags', 'fixture:tags.json')
+    cy.intercept('GET', '**/tags', {fixture: 'tags.json'})
     cy.loginToApp()
   })
 
   it('verify correct request and responce', () => {
-    //server() and route() The old syntax that deprecated in Cypress 6
-    cy.server()
-    cy.route('POST', '**/articles').as('postArticles')
+
+    cy.intercept('POST', '**/articles').as('postArticles')
 
     cy.contains('New Article').click()
     cy.get('[formcontrolname="title"]').type('Article Title')
@@ -23,7 +21,7 @@ describe('Test with backend', () => {
     cy.get('@postArticles').then(xhr => {
       console.log(xhr)
 
-      expect(xhr.status).to.equal(200)
+      expect(xhr.response.statusCode).to.equal(307)
       expect(xhr.request.body.article.body).to.equal('Article body text')
       expect(xhr.request.body.article.description).to.equal('Article description text')
     })
@@ -38,9 +36,9 @@ describe('Test with backend', () => {
       .and('contain', 'testing')
   });
 
-  it.only('verify global feed likes count', () => {
-    cy.route('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
-    cy.route('GET', '**/articles*', 'fixture:articles.json')
+  it('verify global feed likes count', () => {
+    cy.intercept('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
+    cy.intercept('GET', '**/articles*', {fixture: 'articles.json'})
 
     cy.contains('Global Feed').click()
     cy.get('app-article-list button').then(articleList => {
@@ -50,7 +48,7 @@ describe('Test with backend', () => {
 
     cy.fixture('articles').then(file => {
       const articleLink = file.articles[1].slug
-      cy.route('POST', '**/articles/' + articleLink + '/favorite', file);
+      cy.intercept('POST', '**/articles/' + articleLink + '/favorite', file);
     })
 
     // cy.get('app-article-list button').eq(1).click().should('contain','101')
